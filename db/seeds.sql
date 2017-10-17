d82520e44ae40939410f843379b76a44ba68aeb7
Ryan Franco <rfranco92@gmail.com>
2017-10-16 20:32:00 -0400

    started new file

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, date) VALUES ('Good Burger');
+INSERT INTO burgers (burger_name, date) VALUES ('Royale With Cheese');
+INSERT INTO burgers (burger_name, date) VALUES ('Death');
